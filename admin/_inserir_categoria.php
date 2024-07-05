9eeda1792d5b5ad18e05ab0fd7de10252826ec4c
GuhStvo <gustavo.asilva86@senacsp.edu.br>
2024-07-05 16:21:23 -0300

    Adicionando mais estilos ao projeto

--- admin/_inserir_categoria.php
+++ admin/_inserir_categoria.php
@@ -1,4 +1,8 @@
 <!-- _inserir_categoria.php -->
+<html>
+<head>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
 
 <?php
 include 'conexao.php';
@@ -10,7 +14,13 @@ $inserir = mysqli_query($conexao,$sql);
 // mysqli_query é a função do PHP para executar um query
 ?>
 
-<center>
-    <h4>Categoria Adicionada com Sucesso</h4>
-    <a href="index.php" role="button">Voltar</a>
-</center>
+
+    <div class="container">
+        <center>
+            <h4 class="alert alert-success" role="alert">Categoria Adicionada com Sucesso</h4>
+            <a class="btn btn-primary" href="index.php" role="button">Voltar</a>
+        </center>
+    </div>
+
+
+</html>
--- admin/inserir_categoria.php
+++ admin/inserir_categoria.php
@@ -4,13 +4,18 @@
 <head>
     <meta charset="utf-8">
     <title>Cadastro de categorias - Inclusão</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
-<body>
-    <h4>Inclusão de Categorias</h4>
-    <form action="_inserir_categoria.php" method="post"> 
-        <label>Descrição</label>
-        <input type="text" id="desc_categoria" name="desc_categoria" required>
-        <button type="submit" id="botao">Cadastrar</button>
-    </form>
+<body style="margin: 1%;">
+    <div class="container">
+        <h4 style="margin-bottom: 15px;">Inclusão de Categorias</h4>
+        <form action="_inserir_categoria.php" method="post">
+            <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="desc_categoria floatingInput" name="desc_categoria" required placeholder="name@example.com">
+                <label for="floaringInput">Descrição</label>
+                <button style="margin-top: 15px;" type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+        </form>
+    </div>
 </body>
 </html>
